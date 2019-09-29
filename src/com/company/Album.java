092bc3cfe52dff3f17a81b228ee8d7b2a1f9c17b
inji <blueviolet486@gmail.com>
2019-09-29 19:30:03 +0400

    Inner class is created

--- src/com/company/Album.java
+++ src/com/company/Album.java
@@ -7,49 +7,68 @@ import java.util.List;
 public class Album {
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
 
-    //to check we're not duplicating songs
-    public boolean addSong(String title, double duration){
-        if (findSong(title) == null){
-            this.songs.add(new Song(title, duration));
-            return true;
-        }
-        return false;
+    public boolean addSong(String title, double duration) {
+        return this.songs.add(new Song(title, duration));
     }
 
-    private Song findSong(String title){
-        for (Song checkedSong: this.songs){
-            if (checkedSong.getTitle().equals(title)){
-                return checkedSong;
-            }
-        }
-        return null;
-    }
-
-    public boolean addToPlaylist(int trackNumber, List<Song> playList){
-        int index = trackNumber -1;
-        if ((index >= 0) && (index <= this.songs.size())){
-            playList.add(this.songs.get(index));
+    public boolean addToPlaylist(int trackNumber, List<Song> playList) {
+        Song checkedSong = this.songs.findSong(trackNumber);
+        if (checkedSong != null){
+            playList.add(checkedSong);
             return true;
         }
         System.out.println("This album does not have a track " + trackNumber);
         return false;
     }
 
-    public boolean addToPlaylist(String title, List<Song> playList){
-        Song checkedSong = findSong(title);
-        if (checkedSong != null){
+    public boolean addToPlaylist(String title, List<Song> playList) {
+        Song checkedSong = songs.findSong(title);
+        if (checkedSong != null) {
             playList.add(checkedSong);
             return true;
         }
         System.out.println("The song " + title + " is not in this album.");
         return false;
+    }
+
+    private class SongList {
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<Song>();
+        }
+
+        public boolean add(Song song) {
+            if (songs.contains(song)) {
+                return false;
+            }
+            this.songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for (Song checkedSong : this.songs) {
+                if (checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+            int index = trackNumber - 1;
+            if ((index >= 0) && (index < songs.size())) {
+                return songs.get(index);
+            }
+            return null;
+        }
     }
 }
